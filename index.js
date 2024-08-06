4479ace6038709cb13d4c3124e1d9d4e3f728dfc
rko16 <ramkrishnaorton@gmail.com>
2024-08-06 19:48:31 +0530

    changesss

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
-
 const app = express();
 app.use(express.static("public"));
-
 const upload = multer({ dest: "uploads/" });
-
 app.post("/upload", upload.single("file"), (req, res) => {
   try {
     const { path: filePath, originalname: fileName } = req.file;
     const fileContent = fs.readFileSync(filePath, "utf-8");
-
-    const { questions, options } = extractData(fileContent);
-
+    const { questions, options, answers, explanations } =
+      extractData(fileContent);
     const responseObject = {
       fileName,
       questions,
       options,
+      answers,
+      explanations,
     };
-
     fs.unlinkSync(filePath);
-
     // Send the JSON data as a downloadable file
     res.setHeader(
       "Content-disposition",
@@ -34,40 +30,36 @@ app.post("/upload", upload.single("file"), (req, res) => {
     res.status(500).json({ error: "Failed to process text file" });
   }
 });
-
 function extractData(fileContent) {
-  // Regular expression to match questions and options with different formats
   const questionRegex =
     /(\d+\.)\s+(.*?)\s*(?:\n|$)((?:\(?[a-d]\)?[>\)]?\s.*?\n?)+)(?:\n|$)/gi;
-
+  // const questionRegex = /Q(\d+)\.\s+(.*?)\s+a\>(.*?)\s+b\>(.*?)\s+c\>(.*?)\s+d\>(.*?)\s+Answer:\s+\((\w)\)\s+(.*)\s+Explanation:\s+(.*)/gi;
+  // const questionRegex = /(\d+)\.\s+(.*?)\s+\(a\)(.*?)\s+\(b\)(.*?)\s+\(c\)(.*?)\s+\(d\)(.*?)/gi;
   const matches = [...fileContent.matchAll(questionRegex)];
-
   const questions = [];
   const options = [];
-
+  const answers = [];
+  const explanations = [];
   for (const match of matches) {
-    const [, questionNum, question, optionsBlock] = match;
-    const optionsArray = optionsBlock
-      .split("\n")
-      .filter(Boolean)
-      .map((opt) => opt.trim());
-
+    const [
+      ,
+      questionNum,
+      question,
+      optionA,
+      optionB,
+      optionC,
+      optionD,
+      answerCode,
+      answer,
+      explanation,
+    ] = match;
     questions.push({ questionNum, question });
-    options.push({
-      optionA: getOptionText(optionsArray[0]),
-      optionB: getOptionText(optionsArray[1]),
-      optionC: getOptionText(optionsArray[2]),
-      optionD: getOptionText(optionsArray[3]),
-    });
+    options.push({ optionA, optionB, optionC, optionD });
+    answers.push({ answerCode, answer });
+    explanations.push({ explanation });
   }
-
-  return { questions, options };
+  return { questions, options, answers, explanations };
 }
-
-function getOptionText(option) {
-  return option.replace(/^\(?[a-d]\)?[>\)]?\s*/, "").trim();
-}
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
